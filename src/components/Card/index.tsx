157391aa813bea224fe0d89cc4d569ab4889e756
Victor Paranhos <victor_paranhos@hotmail.com>
2023-12-14 09:48:56 -0300

    add

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,90 +1,90 @@
-import {CardContainer, Buy, Action} from './style'
-import {useEffect, useState} from 'react'
+import { CardContainer, Buy, Action } from './style'
+import { useEffect, useState } from 'react'
 import { Check, Minus, Plus, ShoppingCart } from "@phosphor-icons/react";
 import { useTheme } from 'styled-components';
 import { PropsDataInfo } from '../../hooks/context';
 import { useCart } from '../../hooks/context';
 export interface PropsCard {
-    datainfo: PropsDataInfo,
+  datainfo: PropsDataInfo,
 
 }
 
-export function Card({datainfo}: PropsCard){
-    const { dispatch } = useCart()
-    const theme = useTheme()
-    const numero = datainfo.price;
-    const formatoPadrao = numero.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const numeroFormatado = formatoPadrao.replace('R$', '').trim();
-    
-      const [isActive, setisActive] = useState<boolean>(false);
-      const [dataValue, setdataValue] = useState(1);
-    
+export function Card({ datainfo }: PropsCard) {
+  const { dispatch } = useCart()
+  const theme = useTheme()
+  const numero = datainfo.price;
+  const formatoPadrao = numero.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  const numeroFormatado = formatoPadrao.replace('R$', '').trim();
 
-      function handleSelectCard() {
-        setisActive(true)
-        setdataValue(1)
-        dispatch({
-          type: 'ADD_CART',
-          payload: {
-            cartItems: [{...datainfo, quantity: dataValue}]
-          }
-        });
-   
+  const [isActive, setisActive] = useState<boolean>(false);
+  const [dataValue, setdataValue] = useState(1);
+
+
+  function handleSelectCard() {
+    setisActive(true)
+    setdataValue(1)
+    dispatch({
+      type: 'ADD_CART',
+      payloads: {
+        items: { ...datainfo, quantity: dataValue }
       }
+    });
+
+  }
 
 
-      const decrementValue = () => {
-        const min = Math.max(dataValue - 1, 1)
-        setdataValue(min);
+  const decrementValue = () => {
+    const min = Math.max(dataValue - 1, 1)
+    setdataValue(min);
 
-      };
-    
-      const incrementValue = () => {
-        const max = dataValue + 1
-        setdataValue(max);
-     
-      };
+  };
 
+  const incrementValue = () => {
+    const max = dataValue + 1
+    setdataValue(max);
 
-      useEffect(()=>{
- 
-        let timerout: number
-        if(isActive){
-            timerout = setTimeout(()=>{
-
-                setisActive(false)
-       
-            }, 500)
-        }
-
-        return ()=> {
-            clearInterval(timerout)
-        }
-
-      },[isActive])
+  };
 
 
-    return(
-        <CardContainer>
-        <img src={(datainfo.urlimg)} alt={datainfo.title} />
-        <div>
-          {datainfo.tag.map((tag, index )=> {
-                return <span key={String(index)}>{tag}</span>
-            })}
-        </div>
-        <h1>{datainfo.title}</h1>
-        <label>{datainfo.description}</label>
-        <Buy>
-            <p>R$<strong>{numeroFormatado}</strong> </p>
-            <Action>
-                <div>
-                    <button name='buttonminus' onClick={decrementValue} type="button"><Minus weight='bold' size={14} /></button>
-                    <input type="text" readOnly disabled value={dataValue} />
-                    <button name='buttonmax' onClick={incrementValue} type="button"><Plus weight='bold' size={14} /></button>
-                </div>
-                <button onClick={handleSelectCard} style={isActive ? {background: theme['yellow-dark']}: {}} disabled={isActive}>{isActive ? <Check weight="bold"size={22} />:<ShoppingCart size={22} weight="fill" />}</button>
-            </Action>
-        </Buy>
+  useEffect(() => {
+
+    let timerout: number
+    if (isActive) {
+      timerout = setTimeout(() => {
+
+        setisActive(false)
+
+      }, 500)
+    }
+
+    return () => {
+      clearInterval(timerout)
+    }
+
+  }, [isActive])
+
+
+  return (
+    <CardContainer>
+      <img src={(datainfo.urlimg)} alt={datainfo.title} />
+      <div>
+        {datainfo.tag.map((tag, index) => {
+          return <span key={String(index)}>{tag}</span>
+        })}
+      </div>
+      <h1>{datainfo.title}</h1>
+      <label>{datainfo.description}</label>
+      <Buy>
+        <p>R$<strong>{numeroFormatado}</strong> </p>
+        <Action>
+          <div>
+            <button name='buttonminus' onClick={decrementValue} type="button"><Minus weight='bold' size={14} /></button>
+            <input type="text" readOnly disabled value={dataValue} />
+            <button name='buttonmax' onClick={incrementValue} type="button"><Plus weight='bold' size={14} /></button>
+          </div>
+          <button onClick={handleSelectCard} style={isActive ? { background: theme['yellow-dark'] } : {}} disabled={isActive}>{isActive ? <Check weight="bold" size={22} /> : <ShoppingCart size={22} weight="fill" />}</button>
+        </Action>
+      </Buy>
     </CardContainer>
-    )
+  )
 }
--- src/hooks/context.tsx
+++ src/hooks/context.tsx
@@ -9,86 +9,98 @@ import Havaiano from '../assets/Havaiano.svg'
 //import { produce } from "immer";
 
 export const DataCoffeeInfo = [
-    {
-      id:String(1),
-      urlimg: CafeGelado,
-      tag: ['Tradicional', 'Tag2'],
-      title: 'Title1',
-      description: 'O tradicional café feito com água quente e grãos moídos',
-      price: 9.99,
-    },
-    {
-      id: String(2),
-      urlimg: Americano,
-      tag: ['Tag1', 'Tag2'],
-      title: 'Title2',
-      description: 'Expresso diluído, menos intenso que o tradicional',
-      price: 15.99,
-    },
-    {
-      id: String(3),
-      urlimg: CafeComLeite,
-      tag: ['Tag1', 'Tag2'],
-      title: 'Title10',
-      description: 'Café expresso tradicional com espuma cremosa',
-      price: 29.99,
-    },
-    {
-      id: String(4),
-      urlimg: Capuccino,
-      tag: ['Tag1', 'Tag2'],
-      title: 'Title10',
-      description: 'Meio a meio de expresso tradicional com leite vaporizado',
-      price: 29.99,
-    },
-    {
-      id: String(5),
-      urlimg: Cubano,
-      tag: ['Tag1', 'Tag2'],
-      title: 'Latte',
-      description: 'Uma dose de café expresso com o dobro de leite e espuma cremosa',
-      price: 29.99,
-    },
-    {
-      id: String(6),
-      urlimg: Havaiano,
-      tag: ['Tag1', 'Tag2'],
-      title: 'Title10',
-      description: 'Description10',
-      price: 29.99,
-    },
-    {
-      id: String(7),
-      urlimg: ExpressoCremoso,
-      tag: ['Tag1', 'Tag2', 'tag3', 'Tag1', 'Tag2', 'tag3'],
-      title: 'Title10',
-      description: 'Description10',
-      price: 29.99,
-    }]
+  {
+    id: String(1),
+    urlimg: CafeGelado,
+    tag: ['Tradicional', 'Tag2'],
+    title: 'Title1',
+    description: 'O tradicional café feito com água quente e grãos moídos',
+    price: 9.99,
+  },
+  {
+    id: String(2),
+    urlimg: Americano,
+    tag: ['Tag1', 'Tag2'],
+    title: 'Title2',
+    description: 'Expresso diluído, menos intenso que o tradicional',
+    price: 15.99,
+  },
+  {
+    id: String(3),
+    urlimg: CafeComLeite,
+    tag: ['Tag1', 'Tag2'],
+    title: 'Title10',
+    description: 'Café expresso tradicional com espuma cremosa',
+    price: 29.99,
+  },
+  {
+    id: String(4),
+    urlimg: Capuccino,
+    tag: ['Tag1', 'Tag2'],
+    title: 'Title10',
+    description: 'Meio a meio de expresso tradicional com leite vaporizado',
+    price: 29.99,
+  },
+  {
+    id: String(5),
+    urlimg: Cubano,
+    tag: ['Tag1', 'Tag2'],
+    title: 'Latte',
+    description: 'Uma dose de café expresso com o dobro de leite e espuma cremosa',
+    price: 29.99,
+  },
+  {
+    id: String(6),
+    urlimg: Havaiano,
+    tag: ['Tag1', 'Tag2'],
+    title: 'Title10',
+    description: 'Description10',
+    price: 29.99,
+  },
+  {
+    id: String(7),
+    urlimg: ExpressoCremoso,
+    tag: ['Tag1', 'Tag2', 'tag3', 'Tag1', 'Tag2', 'tag3'],
+    title: 'Title10',
+    description: 'Description10',
+    price: 29.99,
+  }]
 
 interface PropsContextCart {
-    children: ReactNode
+  children: ReactNode
+}
+
+
+interface PropsDataInfo2 {
+  id: string,
+  urlimg: string,
+  tag: string[],
+  title: string,
+  description: string,
+  price: number,
+  quantity: number
 }
 
 export interface PropsDataInfo {
-    id: string,
-    urlimg: string,
-    tag: string[],
-    title: string,
-    description: string,
-    price: number,
-    quantity?: number
+  id: string,
+  urlimg: string,
+  tag: string[],
+  title: string,
+  description: string,
+  price: number,
+  quantity?: number
 }
+
 interface PropsActionReducer {
   type: string,
-  payload: {
-    cartItems: PropsDataInfo[]
+  payloads: {
+    items: PropsDataInfo2
   }
 }
 
 interface PropsDataContext {
-    DataCoffeeInfo: PropsDataInfo[],
-    dispatch: React.Dispatch<PropsActionReducer>;
+  DataCoffeeInfo: PropsDataInfo[],
+  dispatch: React.Dispatch<PropsActionReducer>;
 }
 
 
@@ -97,56 +109,69 @@ const CartContext = createContext({} as PropsDataContext)
 
 interface PropsReducerCart {
   type: string,
-  payload: {
-    cartItems: PropsDataInfo[]
+  payloads: {
+    items: PropsDataInfo2[],
+
   }
 
 }
 
-function CartProvider({children}: PropsContextCart){
+function CartProvider({ children }: PropsContextCart) {
 
 
   const [ExemploDispatch, dispatch] = useReducer((state: PropsReducerCart, action: PropsActionReducer) => {
 
+    if (action.type === 'ADD_CART') {
+      const verify = state.payloads.items.findIndex(dataDispatch => dataDispatch.id === action.payloads.items.id);
 
-    if(action.type === 'ADD_CART'){
+      if (verify > -1) {
+        // Se o item já existe no carrinho, atualize a quantidade
+        const newArray = [...state.payloads.items];
+        newArray[verify].quantity += action.payloads.items.quantity;
 
-      return {
-        ...state,
-        payload: {
-          cartItems: [...state.payload.cartItems, action.payload.cartItems[0]]
-        }
- 
+        return {
+          ...state,
+          payloads: {
+            items: newArray,
+          },
+        };
+      } else {
+        // Se o item não existe no carrinho, adicione-o
+        return {
+          ...state,
+          payloads: {
+            items: [...state.payloads.items, action.payloads.items],
+          },
+        };
       }
-    
-
-
     }
-    
-    
-  
+
+
     return state;
 
-    
   }, {
-      type: '',
-      payload: {
-        cartItems: []
-      }    
-});
-console.log(ExemploDispatch)
+    type: '',
+    payloads: {
+      items: []
+    }
+  });
 
 
-    return(
-        <CartContext.Provider value={{DataCoffeeInfo, dispatch}}>
-            {children}
-        </CartContext.Provider>
-    )
+  console.log(ExemploDispatch)
+  const { payloads: { items } } = ExemploDispatch
+  console.log(items)
+
+
+  return (
+    <CartContext.Provider value={{ DataCoffeeInfo, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  )
 }
 
-function useCart(){
-    const context = useContext(CartContext)
-    return context
+function useCart() {
+  const context = useContext(CartContext)
+  return context
 }
 
-export {CartProvider, useCart}
+export { CartProvider, useCart }
